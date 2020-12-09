000456e4c9e69dceebd3b660b5e5ef0c1b7cdb1c
Henry Munoz <alexcol123@yahoo.com>
2020-12-09 17:45:31 -0500

    aded products and product pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Switch, Route } from 'react-router-dom'
 
 import './App.scss'
 import HomePage from './components/home-page'
+import NotFound from './components/NotFound'
+import Shop from './components/pages/shop/Shop'
 
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={Shop} />
+        <Route path='*' component={NotFound} />
       </Switch>
     </div>
   )
