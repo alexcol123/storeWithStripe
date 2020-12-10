74c87cc3fbad36c9d7de1b897013f253cba48619
Henry Munoz <alexcol123@yahoo.com>
2020-12-09 22:18:14 -0500

    added single product page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { Switch, Route } from 'react-router-dom'
-
-import './App.scss'
 import HomePage from './components/home-page'
 import NotFound from './components/NotFound'
 import Shop from './components/pages/shop/Shop'
+import SingleProduct from './components/single-product/SingleProduct'
+
+import './App.scss'
 
 function App() {
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={Shop} />
+        <Route path='/shop' component={Shop} />
+        <Route path='/product/:id' component={SingleProduct} />
         <Route path='*' component={NotFound} />
       </Switch>
     </div>
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './footer.styles.scss'
 
 const footer = () => {
-  const year = new Date().getFullYear
+  const year = new Date().getFullYear()
   return <div className='footer'>{year} &copy; NOMAD Store</div>
 }
 
